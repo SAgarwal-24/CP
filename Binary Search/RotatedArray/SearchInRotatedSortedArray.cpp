0a7ac48186bf2cde4f2f5e0724526e1579b40754
Shubham Agarwal <44989343+SAgarwal-24@users.noreply.github.com>
2023-08-12 00:57:19 +0530

    Create SearchInRotatedSortedArray.cpp

---
+++ Binary Search/RotatedArray/SearchInRotatedSortedArray.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    #define pb push_back
+    #define vi vector<int>
+    #define all(x) (x).begin(), (x).end()
+    #define sz(x) ((ll)(x).size())
+    #define watch(x) cout << (#x) << " -> "  << (x) << endl
+    
+    int search(vi& a, int target) {
+        
+        int n = a.size();
+        
+        int lo = 0,  hi = n-1;
+        
+        while(lo <= hi ){
+            
+            int mid = lo + (hi-lo)/2;
+            
+            if( a[mid] == target)
+                return mid;
+            
+            if(a[lo] <= a[mid]){ // range -> [lo, mid] is properly sorted (standard scenario)
+                
+                if(a[lo] <= target &&  target < a[mid]){ // just like standard binary search scenario - no rotation in array 
+                    hi = mid - 1;
+                }
+                else{ 
+                    lo = mid + 1;
+                }
+                
+            }
+            else{
+                
+                if(a[mid]<target && target <=a[hi]){ // range -> [mid+1, target] is properly sorted (standard scenario) -  - no rotation in array 
+                    lo = mid + 1;
+                }
+                else{
+                    hi = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
